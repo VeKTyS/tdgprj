57c66e8444efdd1cd64b8d56d90080bb1fdb673b
abc <abc@example.com>
2025-03-12 10:37:24 +0100

    refonte du graphe avec un nouvelle gestion d'alpha et omega, rajout des successeurs et prédecesseurs associés

--- main.py
+++ main.py
@@ -19,6 +19,11 @@ def constrain_table_reader(constrain_table):
     duration = list()
     constrains = list()
     table = [task, duration, constrains]
+
+    task.append('0') # Alpha
+    duration.append('0') # Alpha
+    constrains.append([]) # Alpha
+
     with open(constrain_table, "r") as f:
         lines = f.readlines()
         for i in range(len(lines)):
@@ -26,8 +31,17 @@ def constrain_table_reader(constrain_table):
         for i in range(len(lines)):
             duration.append(lines[i].split()[1])
         for i in range(len(lines)):
-            constrains.append(lines[i].split()[2:])
-    
+            constraint = lines[i].split()[2:]
+            if not constraint:
+                constraint = ['0']
+            constrains.append(constraint)
+
+    task.append(str(len(lines) + 1)) # Omega
+    duration.append('0') # Omega
+    # Omega
+    omega_constraints = [t for t in task if t not in [item for sublist in constrains for item in sublist] and t not in ['0', str(len(lines) + 1)]]
+    constrains.append(omega_constraints)
+
     # Save the table to the memoire file
     with open(memoire_file, "w") as f:
         f.write(str(table))
@@ -59,14 +73,14 @@ def get_predecessors(tasks, constraints):
 def display_graph(constrain_table):
     table = constrain_table_reader(constrain_table)
     tasks, durations, constraints = table
-    N = len(tasks)
+    N = len(tasks) # Nombre de tâches 
     graph = np.zeros((N + 2, N + 2), dtype=int)
     
     for i, task in enumerate(tasks):  
         for constrained_task in constraints[i]:  # Chaque tâche dont i dépend
             if constrained_task in tasks:
                 constrained_index = tasks.index(constrained_task)  # Trouver son index
-                graph[i + 1, constrained_index + 1] = int(durations[constrained_index])  # Associer la bonne durée
+                graph[i, constrained_index] = int(durations[constrained_index])  # Associer la bonne durée
     
     # Ajout des nœuds fictifs 0 et N+1
     for i in range(1, N + 1):
@@ -77,13 +91,14 @@ def display_graph(constrain_table):
     graph = graph.T
 
     # Création des en-têtes
-    headers = ['0'] + [chr(65 + i) for i in range(N)] + [str(N + 1)]
+    #headers = ['0'] + [chr(65 + i) for i in range(N)] + [str(N + 1)]
+    headers = ['0'] + [chr(65 + i) for i in range(N-2)] + [str(N-1)]
 
     # Affichage des en-têtes avec alignement
     print("    " + " ".join(f"{h:>3}" for h in headers))
-    for i in range(N + 2):
+    for i in range(N):
         row = []
-        for j in range(N + 2):
+        for j in range(N):
             if graph[i, j] == 0 and i != 0 and j != N + 1:
                 row.append("  *")
             else:
